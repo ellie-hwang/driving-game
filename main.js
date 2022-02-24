49ce82ca8896cff57a81b3104aa1d3c0060c8f1c
Eleonora Hwang <eleonorahwang@gmail.com>
2022-02-24 15:15:04 -0800

    data.orientation is updated each time car turns

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ document.addEventListener('keydown', turnCar);
 function turnCar(event) {
   if (event.keyCode === 37) /* left */ {
     $racecar.className = 'west';
+    data.orientation = 'west';
   } else if (event.keyCode === 38) /* up */ {
     $racecar.className = 'north';
+    data.orientation = 'north';
   } else if (event.keyCode === 39) /* right */ {
     $racecar.className = 'east';
+    data.orientation = 'east';
   } else if (event.keyCode === 40) /* down */ {
     $racecar.className = 'south';
+    data.orientation = 'south';
   }
 }
